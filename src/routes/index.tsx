b8f570c97d38ae0b902fb95564ee1b44b33b575f
Gabriel <alecksandrov15@gmail.com>
2025-06-04 20:08:54 +0300

    chore: updated import pages links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
-import { RegisterPage } from '@/pages/Authorization/RegisterPage';
-import { LoginPage } from '@/pages/Authorization/LoginPage';
-import { ConstructorPage } from '@/pages/ConstructorPage';
 import { createBrowserRouter } from 'react-router-dom';
-import { NotFoundPage } from '@/pages/NotFoundPage';
-import { LibraryPage } from '@/pages/LibraryPage';
-import { RootLayout } from '@/pages/RootLayout';
-import { LobbyPage } from '@/pages/LobbyPage';
-import { HomePage } from '@/pages/HomePage';
+
+import {
+  ConstructorPage,
+  RegisterPage,
+  NotFoundPage,
+  LibraryPage,
+  RootLayout,
+  LobbyPage,
+  LoginPage,
+  HomePage,
+} from '@/pages';
 
 export const router = createBrowserRouter([
   {
@@ -14,20 +17,16 @@ export const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
         path: '/login',
         element: <LoginPage />,
       },
       {
         path: '/register',
         element: <RegisterPage />,
-      },
-      {
-        path: '*',
-        element: <NotFoundPage />,
-      },
-      {
-        path: '/',
-        element: <HomePage />,
       },
       {
         path: '/lobby',
@@ -40,6 +39,10 @@ export const router = createBrowserRouter([
       {
         path: '/constructor',
         element: <ConstructorPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
   },
