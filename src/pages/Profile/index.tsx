1a5415b0c2fe87972b3539e6406fd14c37afccf4
e-vasilyev <e-vasilyev@asoft.local>
2025-09-01 16:46:23 +0300

    chore: стили

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -5,7 +5,6 @@ import {useEffect, useState} from "react";
 import {ProfileService} from "@/services/profileService.ts";
 import {useParams} from "react-router-dom";
 import type {TUser} from "@/models/User";
-import {Button} from "@/components/Button/Button.tsx";
 
 export function ProfilePage() {
     const [user, setUser] = useState<TUser | null>(null);
@@ -28,14 +27,21 @@ export function ProfilePage() {
     return (
         <div className='profile-page'>
             <div className='general-info'>
-                <div className='profile-image-container'>
+                <div className='profile-image-container'
+                     style={{
+                         //@ts-ignore
+                         "--bg-img": `url(${user?.profilePicture ?? '/src/assets/images/user_filler.png'})`
+                     }}>
                     {
-                        loading ? <div className='loading'></div> : <img className={user?.profilePicture ? '' : 'no-photo'} src={user?.profilePicture ?? '/src/assets/images/user_filler.png'} alt='profile picture'/>
+                        loading ? <div className='loading'></div> :
+                            <img className={user?.profilePicture ? '' : 'no-photo'}
+                                 src={user?.profilePicture ?? '/src/assets/images/user_filler.png'}
+                                 alt='profile picture'/>
                     }
                 </div>
                 <div className='profile-info'>
                     {
-                        loading ? <div className='loading'></div> : <h1>{user ? user.name : 'Пользователь не найден'}</h1>
+                        loading ? <h1></h1> : <h1>{user ? user.name : 'Пользователь не найден'}</h1>
                     }
                 </div>
             </div>
@@ -43,23 +49,78 @@ export function ProfilePage() {
                 <div className='left'>
                     <div className='profile-tokens'>
                         <div className='header'>
-                            <h2>Коллекция</h2>
-                            <Button color={"white"}>Посмотреть все</Button>
+                            <h2>Коллекция персонажей</h2>
                         </div>
                         <div className='content'>
                             <div className='token'>
-                                <img src='/src/assets/images/knight.png'></img>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
                             </div>
                             <div className='token'>
-                                <img src='/src/assets/images/knight.png'></img>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
                             </div>
                             <div className='token'>
-                                <img src='/src/assets/images/knight.png'></img>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='profile-tokens'>
+                        <div className='header'>
+                            <h2>Коллекция предметов</h2>
+                        </div>
+                        <div className='content'>
+                            <div className='token'>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
+                            </div>
+                            <div className='token'>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
+                            </div>
+                            <div className='token'>
+                                <div className='image-container'
+                                     style={{
+                                         //@ts-ignore
+                                         "--bg-img": `url(/src/assets/images/knight.png)`
+                                     }}>
+                                    <img src='/src/assets/images/knight.png'></img>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='right'>online</div>
+                <div className='right'>
+                    <span>Сыграно игр: 2</span>
+                    <span>Добавлено токенов: 22</span>
+                    <span>Что-то там еще: 222</span>
+                </div>
             </div>
         </div>
     );
